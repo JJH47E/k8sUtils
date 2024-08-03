68a2657099e1af6b2cca2515ea0515e3d96cf784
JJH <62762383+JJH47E@users.noreply.github.com>
2024-08-03 17:36:03 +0100

    more tests

--- K8sUtils.UnitTests/Services/KubectlServiceTests.cs
+++ K8sUtils.UnitTests/Services/KubectlServiceTests.cs
@@ -1,4 +1,5 @@
 using FluentAssertions;
+using K8sUtils.Exceptions;
 using K8sUtils.ProcessHosts;
 using K8sUtils.Services;
 using NSubstitute;
@@ -29,5 +30,76 @@ public class KubectlServiceTests
         
         // Assert
         await act.Should().ThrowAsync<ArgumentException>().WithMessage("Namespace cannot be null or empty*");
+    }
+    
+    [Fact]
+    public async Task GetPodsAsync_NoPodsFound_ThrowsKubectlRuntimeException()
+    {
+        // Arrange
+        _kubectlHost.ListPods(Arg.Any<string>()).Returns([]);
+
+        // Act
+        var act = async () => await _sut.GetPodsAsync("test");
+
+        // Assert
+        await act.Should().ThrowAsync<KubectlRuntimeException>().WithMessage("Unable to find any pods in the namespace*");
+    }
+
+    [Fact]
+    public async Task GetPodsAsync_PodsFound_ReturnsPods()
+    {
+        // Arrange
+        string[] expectedPods = ["pod1", "pod2"];
+        _kubectlHost.ListPods(Arg.Any<string>()).Returns(expectedPods);
+        
+        // Act
+        var result = await _sut.GetPodsAsync("test");
+        
+        // Assert
+        result.Should().BeEquivalentTo(expectedPods);
+        await _kubectlHost.Received(1).ListPods(Arg.Any<string>());
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task GetLogsAsync_PodNameNotProvided_ThrowsArgumentException(string? podName)
+    {
+        // Arrange
+        // Act
+        var act = async () => await _sut.GetLogsAsync(podName, "namespace");
+        
+        // Assert
+        await act.Should().ThrowAsync<ArgumentException>().WithMessage("Pod name cannot be null or empty*");
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task GetLogsAsync_NamespaceNotProvided_ThrowsArgumentException(string? @namespace)
+    {
+        // Arrange
+        // Act
+        var act = async () => await _sut.GetLogsAsync("pod", @namespace);
+        
+        // Assert
+        await act.Should().ThrowAsync<ArgumentException>().WithMessage("Namespace cannot be null or empty*");
+    }
+    
+    [Fact]
+    public async Task GetLogsAsync_ArgsProvided_CallsHostAndReturnsLogs()
+    {
+        // Arrange
+        string[] expectedLogs = ["LOG", "What?"];
+        _kubectlHost.GetLogs(Arg.Any<string>(), Arg.Any<string>()).Returns(expectedLogs);
+        
+        // Act
+        var result = await _sut.GetLogsAsync("test", "ns");
+        
+        // Assert
+        result.Should().BeEquivalentTo(expectedLogs);
+        await _kubectlHost.Received(1).GetLogs(Arg.Any<string>(), Arg.Any<string>());
     }
 }
